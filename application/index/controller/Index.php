42ec5e9749c72847a9233c8245b94c6c9895c167
1226151667 <1226151667@qq.com>
2017-05-08 17:55:18 +0800

    web.html页面鼠标滑过图片有蒙版文字+图片内部放大效果

---
+++ application/index/controller/Index.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+    public function main2()
+    {
+        return view();
+    }
+    public function main3()
+    {
+        return view();
+    }
+    public function main3Tk()
+    {
+        return view();
+    }
+    public function web()
+    {
+        return view();
+    }
+    public function h5()
+    {
+        return view();
+    }
+    public function operate()
+    {
+        return view();
+    }
+    public function contact()
+    {
+        return view();
+    }
+}
---
+++ application/route.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// return [
+//     '__pattern__' => [
+//         'name' => '\w+',
+//     ],
+//     '[hello]'     => [
+//         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//         ':name' => ['index/hello', ['method' => 'post']],
+//     ],
+
+// ];
+return [
+    'web'   => 'index/Index/web',
+    'h5'   => 'index/Index/h5',
+    'operate'   => 'index/Index/operate',
+    'contact'   => 'index/Index/contact',
+];
